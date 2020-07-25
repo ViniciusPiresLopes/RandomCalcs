cc95790aa0b9c0e0f78325f5ead33060d8f48526
Vinícius Pires Lopes <52144908+ViniciusPiresLopes@users.noreply.github.com>
2020-07-25 15:13:21 -0300

    New code

--- run.py
+++ run.py
@@ -1,25 +1,4 @@
-# import modules
-from random_calcs import Program
-import os
+import random_calcs
 
-
-# funcions
-def clear():
-    os.system('clear') or None
-
-
-def title(msg='', s='='):
-    print(s*30)
-    print(f'{msg:^30}')
-    print(s*30)
-
-
-# quantity of calcs
-title(msg='QUANTIDADE')
-quant = int(input('Quantas contas deseja fazer? '))
-clear()
-
-# run program
-program = Program()
-program.run_program(quant_calcs=quant)
-
+app = random_calcs.App()
+app.run()
